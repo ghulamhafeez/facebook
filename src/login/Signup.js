9a61653f71559268d5c2d932070e74abf0283693
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-06-20 21:21:12 +0500

    Create signup screen ui & added link on login page

--- src/login/Signup.js
+++ src/login/Signup.js
@@ -4,11 +4,14 @@ import Box from "@mui/material/Box";
 import { Button, CardActions, CardContent } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
-
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 
-export const LogIn = () => {
+export const SignUp = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
@@ -17,11 +20,15 @@ export const LogIn = () => {
       <Typography sx={{ color: "#1877F2", mt: 5, mb: 4 }} variant="h3">
         <b>facebook</b>
       </Typography>
-      <Card sx={{ maxWidth: 400, minHeight: 340, borderRadius: 2, ml: 72 }}>
-        <Typography sx={{ mt: 2 }} variant="h6">
-          Log in to Facebook
+      <Card sx={{ maxWidth: 400, minHeight: 360, borderRadius: 2, ml: 72 }}>
+        <Typography sx={{ mt: 2 }} variant="h5">
+         <b>Create a new account</b> 
         </Typography>
-        <CardContent sx={{ mt: 2 }}>
+        <Typography sx={{ mt: 1 }} variant="h6">
+          It's quick and easy.
+        </Typography>
+
+        <CardContent sx={{ mt: 1 }}>
           <Box sx={{ width: "100%", display: "flex", gap: 3, mt: -1 }}>
             <Grid>
               <TextField
@@ -48,12 +55,18 @@ export const LogIn = () => {
         >
           <Button
             size="medium"
-            sx={{ bgcolor: "#1877F2", color: "white", width: 350 }}
+            sx={{ bgcolor: "#119F16", color: "white", width: 350 }}
           >
-            <b>Log in</b>
+            <b>Sign Up</b>
           </Button>
         </CardActions>
-        <Link onClick={console.log("called")}>Sign up for Facebook</Link>
+        <Link
+          onClick={() => {
+            // ...process something
+          }}
+        >
+          Already have an account?
+        </Link>
       </Card>
     </Grid>
   );
